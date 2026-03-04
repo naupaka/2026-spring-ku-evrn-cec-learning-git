a3ee2718408fa514f0e686cf2f6a3ae221172a5e
Naupaka Zimmerman <naupaka@gmail.com>
2026-03-04 16:44:46 -0600

    Change year filter

--- code/plot_keeling_curve_ggplot.R
+++ code/plot_keeling_curve_ggplot.R
@@ -15,7 +15,7 @@ mlo_data <- read.csv("data/monthly_in_situ_co2_mlo_cleaned.csv")
 # use a dplyr pipe and filter function to get rid of -99 values
 mlo_data %>%
   filter(CO2 > 0) %>%
-  filter(Yr > 1985) %>%
+  filter(Yr > 1990) %>%
   ggplot(aes(x = Date.1,
              y = CO2)) +
   geom_line()
